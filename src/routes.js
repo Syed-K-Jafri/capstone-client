032e5d29802ffe5f087c7fdd803a0cd817e88722
Syed-K-Jafri <syedjafri1@yahoo.com>
2020-12-22 21:37:38 +0500

    routing after login

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,12 @@ window.jQuery = $;
 window.$ = $;
 global.jQuery = $;
 
+const Dashboard = React.lazy(() => import("./Demo/dashboard/dashboard"));
+const NotFound = React.lazy(() => import("./Demo/NotFound/NotFound"));
+
 const routes = [
+    { path: '/dashboard', exact: true, role:'user', user: true, component: RequireAuth(Dashboard) },
+    { path: '/not-found', exact: true, role:'public', component: RequireAuth(NotFound) }
 ];
 
 export default routes;
