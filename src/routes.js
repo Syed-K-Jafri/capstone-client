d63016fb1fb3840697b85d872a177799c9b60d18
Syed-K-Jafri <syedjafri1@yahoo.com>
2020-12-22 21:39:35 +0500

    questions route

--- src/routes.js
+++ src/routes.js
@@ -9,10 +9,12 @@ global.jQuery = $;
 const Dashboard = React.lazy(() => import("./Demo/dashboard/dashboard"));
 const NotFound = React.lazy(() => import("./Demo/NotFound/NotFound"));
 const Project = React.lazy(() => import("./Demo/Questions/Questions"));
+const Answer = React.lazy(() => import("./Demo/Answers/Answers"));
 
 const routes = [
     { path: '/dashboard', exact: true, role:'user', user: true, component: RequireAuth(Dashboard) },
     { path: '/project/:project', exact: true, role:'user', user: true, component: RequireAuth(Project) },
+    { path: '/project/:project/question/:question', exact: true, role:'user', user: true, component: RequireAuth(Answer) },
     { path: '/not-found', exact: true, role:'public', component: RequireAuth(NotFound) }
 ];
 
